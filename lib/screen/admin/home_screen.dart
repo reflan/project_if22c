9faa37280d46581792929e83dde96d0572a888dd
reflan nuari <reflannuari@gmail.com>
2024-05-04 09:27:00 +0700

    commit

--- lib/screen/admin/home_screen.dart
+++ lib/screen/admin/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:project_if22c/config/asset.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,7 +14,237 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       padding: EdgeInsets.all(10),
-      child: Text("Selamat datang di home screen"),
+      child: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Asset.colorPrimary,
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+            child: Image(
+              image: NetworkImage(
+                  'https://raw.githubusercontent.com/reflan/project_if22c/main/asset/images/header_uti.jpg'),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(
+              top: 25,
+              bottom: 10,
+            ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton('User', Icons.people, 0),
+                    IconButton('Menu 2', Icons.mail, 0),
+                    IconButton('Menu 3', Icons.folder, 0),
+                    IconButton('Menu 4', Icons.person, 0),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton('Menu 5', Icons.ac_unit, 0),
+                    IconButton('Menu 6', Icons.back_hand, 0),
+                    IconButton('Menu 7', Icons.cached, 0),
+                    IconButton('Menu 8', Icons.logout, 0),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              "Berita Terkini",
+              style: TextStyle(
+                color: Asset.colorPrimaryDark,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            alignment: Alignment.topLeft,
+          ),
+          SizedBox(
+            height: 170,
+            child: ListView(
+              children: [
+                CarouselSlider(
+                  items: [
+                    BeritaTerkini(
+                        'https://teknokrat.ac.id/wp-content/uploads/2023/04/Sekar-Kinasih-040423-1.jpg'),
+                    BeritaTerkini(
+                        'https://teknokrat.ac.id/wp-content/uploads/2023/01/WhatsApp-Image-2023-01-04-at-18.00.09.jpeg'),
+                    BeritaTerkini(
+                        'https://teknokrat.ac.id/wp-content/uploads/2023/04/Tim-Tari-Teknokrat-120323.jpeg'),
+                    BeritaTerkini(
+                        'https://teknokrat.ac.id/wp-content/uploads/2023/04/ddaa.png'),
+                    BeritaTerkini(
+                        'https://teknokrat.ac.id/wp-content/uploads/2023/04/Sekar-Kinasih-040423-1.jpg'),
+                  ],
+                  options: CarouselOptions(
+                    height: 170,
+                    enlargeCenterPage: true,
+                    autoPlay: true,
+                    aspectRatio: 16 / 9,
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enableInfiniteScroll: true,
+                    autoPlayAnimationDuration: Duration(
+                      milliseconds: 800,
+                    ),
+                    viewportFraction: 0.8,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class IconButton extends StatelessWidget {
+  final String nameLabel;
+  final IconData iconLabel;
+  final int index;
+
+  IconButton(this.nameLabel, this.iconLabel, this.index);
+
+  List<Map> _fragment = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            // margin: EdgeInsets.only(bottom: 5),
+            child: Material(
+              borderRadius: BorderRadius.all(
+                Radius.circular(40),
+              ),
+              color: Colors.transparent,
+              child: InkWell(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(40),
+                ),
+                onTap: () {},
+                child: Container(
+                  // margin: EdgeInsets.all(5),
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    color: Asset.colorPrimaryDark,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30),
+                    ),
+                  ),
+                  child: Center(
+                    child: Stack(
+                      children: [
+                        Icon(
+                          iconLabel,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 5, bottom: 5),
+            child: Text(
+              nameLabel,
+              style: TextStyle(fontSize: 14),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BeritaTerkini extends StatelessWidget {
+  final String img;
+
+  BeritaTerkini(this.img);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      height: MediaQuery.of(context).size.height,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                image: DecorationImage(
+                  image: NetworkImage(img),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Positioned.fill(
+            child: Container(
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                gradient: LinearGradient(
+                  colors: [
+                    Asset.colorPrimaryDark,
+                    Colors.transparent,
+                  ],
+                  end: Alignment.topCenter,
+                  begin: Alignment.bottomCenter,
+                  stops: [0.1, 0.9],
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Isi Berita yang ada di setiap konten.',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    '31 Maret 2023',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.white,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      // alignment: Alignment(1, 1),
     );
   }
 }
